4f0d800471f2db13058df657c0c4ac77eacf4cea
Liz <bozhkova.elizaveta34@gmail.com>
2023-11-06 14:37:52 +0300

    add new types action

--- src/redux/actions/ActionTypes.js
+++ src/redux/actions/ActionTypes.js
@@ -2,6 +2,7 @@ export const ADD_ITEM_TO_LIST = "ADD_ITEM_TO_LIST";
 export const DELETE_ITEM_FROM_LIST = "DELETE_ITEM_FROM_LIST";
 
 export const FETCH_TODOS = "FETCH_TODOS";
+export const FETCH_TODOS_BY_DATE = "FETCH_TODOS_BY_DATE";
 export const FETCH_DRIVER = "FETCH_DRIVER";
 export const FETCH_USER = "FETCH_USER";
 
