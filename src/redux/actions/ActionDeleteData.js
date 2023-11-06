348912de5c030ca62d2c790cbbe87fa805018c00
Liz <bozhkova.elizaveta34@gmail.com>
2023-11-06 14:37:11 +0300

    delete todos req and new fetch todos

--- src/redux/actions/ActionDeleteData.js
+++ src/redux/actions/ActionDeleteData.js
@@ -1,12 +1,12 @@
 import * as CONSTANTS from './ActionTypes';
 import axios from 'axios';
-import { setLoad } from './ActionFetchData';
+import { fetchTodos } from './ActionFetchData';
 
-export const deleteTodo = (id) => (dispatch) => {
+export const deleteTodo = (id, dateTodos) => (dispatch) => {
     axios.delete(`http://localhost:3001/api/todos/delete/${id}`)
     .then(({ data }) => {
         dispatch(setMessage(data));
-        dispatch(setLoad(false));
+        dispatch(fetchTodos(dateTodos));
     })
 }
 
