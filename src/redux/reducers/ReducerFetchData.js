6a34f0e17261b68686cf482bacfaa56500351812
Liz <bozhkova.elizaveta34@gmail.com>
2023-11-06 14:40:44 +0300

    add todo by day array and new case

--- src/redux/reducers/ReducerFetchData.js
+++ src/redux/reducers/ReducerFetchData.js
@@ -2,6 +2,7 @@ import * as CONSTANTS from '../actions/ActionTypes';
 
 const initialState = {
     todos: [],
+    todosDay: [],
     drivers: [],
     users: {},
     loading: false
@@ -11,6 +12,8 @@ const ReducerFetchData = (state = initialState, action) => {
     switch (action.type) {
         case CONSTANTS.FETCH_TODOS:
             return { ...state, todos: action.payload };
+        case CONSTANTS.FETCH_TODOS_BY_DATE:
+            return { ...state, todosDay: state.todos.filter(el=>new Date(el.dateStart).toDateString() === new Date(action.payload).toDateString())};
         case CONSTANTS.FETCH_DRIVER:
             return { ...state, drivers: action.payload };
         case CONSTANTS.FETCH_USER:
